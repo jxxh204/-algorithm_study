a0fb0d89637d6b77fab479712832a7674b51d984
jxxh204 <ahhancom@gmail.com>
2023-04-19 00:30:13 +0900

    17295 오큰수 시간초과

--- dev.js
+++ dev.js
@@ -1,21 +1,23 @@
 const fs = require("fs");
 // TODO: 제출 시 경로 변환 필수 ("/dev/stdin")
-const input = fs.readFileSync("test.txt").toString();
+const input = fs.readFileSync("test.txt").toString().split("\n");
 function solution(input) {
-  let sticks = [];
-  let result = 0;
-  for (let i = 0; i < input.length; i++) {
-    if (input[i] === "(" && input[i + 1] === ")") {
-      //레이저
-      result += sticks.length;
-      i += 1;
-    } else if (input[i] === "(") {
-      sticks.push(input[i]);
-    } else if (input[i] === ")") {
-      sticks.pop();
-      result += 1;
+  const nums = input[1].split(" ").map((v) => +v);
+  const stack = [];
+  const n = input[1].split(" ");
+  const result = n.fill(-1);
+
+  for (let i = n.length - 1; i > -1; i--) {
+    for (let j = stack.length - 1; j > -1; --j) {
+      if (stack[j] > nums[i]) {
+        result[i] = stack[j];
+        break;
+      } else {
+        result[i] = -1;
+      }
     }
+    stack.push(nums[i]);
   }
-  return result;
+  return result.join(" ");
 }
 console.log(solution(input));
