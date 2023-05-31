988665bb4df91d7805f8173f2ee6a51274e9b60e
JaeHwan <ahhancom@gmail.com>
2023-05-31 16:36:43 +0900

    doublyLinkedList unshift

--- data-structure/LinkedList/doublyLinkedList.js
+++ data-structure/LinkedList/doublyLinkedList.js
@@ -27,6 +27,19 @@ class DoublyLinkedList {
 
     return this;
   }
+  unshift(val) {
+    const newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
   pop() {
     console.log("push");
     if (this.length === 0) return undefined;
