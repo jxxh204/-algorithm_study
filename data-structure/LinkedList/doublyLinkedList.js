97e00ce65a86bb30c1e5d651889c57ea173ecf82
JaeHwan <ahhancom@gmail.com>
2023-06-01 18:00:36 +0900

    get  제작, set 제작 중

--- data-structure/LinkedList/doublyLinkedList.js
+++ data-structure/LinkedList/doublyLinkedList.js
@@ -19,9 +19,15 @@ class DoublyLinkedList {
       this.head = newNode;
       this.tail = newNode;
     } else {
-      this.tail.next = newNode;
-      newNode.prev = this.tail;
-      this.tail = newNode;
+      if (this.length === 1) {
+        this.head.next = newNode;
+        newNode.prev = this.head;
+        this.tail = newNode;
+      } else {
+        this.tail.next = newNode;
+        newNode.prev = this.tail;
+        this.tail = newNode;
+      }
     }
     this.length++;
 
@@ -41,7 +47,6 @@ class DoublyLinkedList {
     return this;
   }
   pop() {
-    console.log("push");
     if (this.length === 0) return undefined;
     if (this.length === 1) {
       this.head = null;
@@ -54,5 +59,70 @@ class DoublyLinkedList {
     }
     this.length--;
     return this;
+  }
+  shift() {
+    console.log("shift");
+    const tempHead = this.head;
+    if (this.length === 0) return undefined;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      const nextNode = this.head.next;
+      nextNode.prev = null;
+      this.head.next = null;
+      this.head = nextNode;
+    }
+    this.length--;
+    return tempHead;
+  }
+  get(index) {
+    if (index >= this.length || index < 0) return null;
+    if (index === 0) return this.head;
+    if (index === this.length - 1) return this.tail;
+
+    const halfLength = Math.floor(this.length / 2);
+    if (index <= halfLength) {
+      //작
+      let count = 0;
+      let countNode = this.head;
+      while (index !== count) {
+        countNode = countNode.next;
+        count++;
+      }
+    } else {
+      //크
+      count = this.length - 1;
+      countNode = this.tail;
+      while (index !== count) {
+        countNode = countNode.prev;
+        count--;
+      }
+    }
+    return countNode;
+  }
+  set(index, val) {
+    let newNode = new Node(val);
+
+    if (index >= this.length) return false;
+    if (index < 0) return false;
+    if (index === 0) {
+      this.head.next.prev = newNode;
+      this.head = newNode;
+      return true;
+    }
+    if (index === this.length - 1) {
+      this.tail.prev.next = newNode;
+      this.tail = newNode;
+      return true;
+    }
+    let getNode = this.get(index);
+
+    const beforeNode = getNode.prev;
+    const afterNode = getNode.next;
+    beforeNode.next = newNode;
+    afterNode.prev = newNode;
+    getNode = newNode;
+    return true;
   }
 }
