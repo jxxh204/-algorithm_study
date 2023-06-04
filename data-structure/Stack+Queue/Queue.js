1bed5be8c1980787afb3ea0dfec5fea086406e45
JaeHwan <ahhancom@gmail.com>
2023-06-04 18:41:30 +0900

    queue

---
+++ data-structure/Stack+Queue/Queue.js
@@ -0,0 +1,36 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (this.size === 0) return false;
+    if (this.size === 1) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return this.size++;
+  }
+  dequeue() {
+    if (this.size === 0) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp;
+  }
+}
