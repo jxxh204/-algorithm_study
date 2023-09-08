f6602f572c3be22c025ac908b708782756aa7233
jxxh204 <ahhancom@gmail.com>
2023-09-08 23:32:35 +0900

    프로그래머스 : 바탕화면 정리

--- dist/test.dev.js
+++ dist/test.dev.js
@@ -1,21 +1,33 @@
 "use strict";
 
-function solution(name, yearning, photo) {
-  var nameObj = {};
-  var 점수 = [];
-  name.map(function (n, index) {
-    nameObj[n] = yearning[index];
-  });
-  photo.map(function (p) {
-    var temp점수 = 0;
-    p.map(function (name) {
-      if (nameObj[name]) temp점수 += nameObj[name];
-    });
-    점수.push(temp점수);
-    temp점수 = 0;
-  });
-  console.log(점수);
-  return 점수;
-}
+function solution(wallpaper) {
+  if (wallpaper.length < 1) return null;
+  if (wallpaper[0].length < 1) return null;
+  var hight = wallpaper.length;
+  var width = wallpaper[0].length;
+  var lx = null,
+      rx = null,
+      ty = null,
+      by = null;
 
-solution(["may", "kein", "kain", "radi"], [5, 10, 1, 3], [["may", "kein", "kain", "radi"], ["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]);
+  for (var i = 0; i < hight; i++) {
+    for (var j = 0; j < width; j++) {
+      if (wallpaper[i][j] === "#") {
+        if (lx === null || rx === null || ty === null || by === null) {
+          lx = j;
+          rx = j;
+          ty = i;
+          by = i;
+        } //  i = y j = x
+
+
+        lx = Math.min(lx, j);
+        rx = Math.max(rx, j);
+        ty = Math.min(ty, i);
+        by = Math.max(by, i);
+      }
+    }
+  }
+
+  return [ty, lx, by + 1, rx + 1];
+}
