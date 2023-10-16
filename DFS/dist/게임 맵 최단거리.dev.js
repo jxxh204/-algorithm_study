9eaa14228bfc982ddd746e0195fd658b8597b915
jxxh204 <ahhancom@gmail.com>
2023-10-16 17:57:46 +0900

    프로그래머스 : 게임 맵 최단거리
    queue를 이용해서 풀이.
    DFS,BFS 공부가 더 필요할듯

--- DFS/dist/게임 맵 최단거리.dev.js
+++ DFS/dist/게임 맵 최단거리.dev.js
@@ -8,43 +8,110 @@ function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) ||
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
-function solution(maps) {
-  var yMax = maps.length - 1;
-  var xMax = maps[0].length - 1;
-  var directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
-  var result = -1;
+function 이동(maps, location, maxX, maxY) {
+  var direction = {
+    LEFT: [0, -1],
+    //y,x
+    RIGHT: [0, 1],
+    DOWN: [1, 0],
+    UP: [-1, 0]
+  };
 
-  function dfs(x, y, count) {
-    //마지막 도달하면 count 리턴
-    if (x >= xMax && y >= yMax) {
-      result = count + 1;
-      return;
-    } //0일 경우 멈춤
+  for (var move in direction) {
+    var 방향 = direction[move];
+    var 현재y = location[0];
+    var 현재x = location[1];
+    var 이동후 = [현재y + 방향[0], 현재x + 방향[1]];
+
+    if (이동후[0] < 0 || 이동후[1] < 0 || 이동후[0] > maxY || 이동후[1] > maxX) {
+      // 음수인지 아닌지 확인.
+      continue;
+    } //두가지 경우의 수가 존재하는 경우 먼저 반복되는 left right 순으로 리턴한다.
 
 
-    if (!maps[y][x]) return; //방문 했던 곳은 발자취를 제거
-
-    maps[y][x] = 0;
-
-    for (var _i = 0, _directions = directions; _i < _directions.length; _i++) {
-      var _directions$_i = _slicedToArray(_directions[_i], 2),
-          dx = _directions$_i[0],
-          dy = _directions$_i[1];
-
-      // 모든 방향 탐색
-      var newX = x + dx;
-      var newY = y + dy;
-
-      if (newX >= 0 && newY >= 0 && maps[newY][newX] === 1) {
-        return dfs(newX, newY, count + 1);
-      }
+    if (maps[이동후[0]][이동후[1]] === 1) {
+      // console.log(maps[이동후[0]][이동후[1]], move);
+      return [이동후[0], 이동후[1]];
+    } else {
+      continue;
     }
   }
+}
 
-  dfs(0, 0, 0);
-  console.log(result);
-  return result;
+function 재귀(maps, location, count) {
+  var maxX = maps[0].length - 1;
+  var maxY = maps.length - 1;
+  var moved = 이동(maps, location, maxX, maxY); // 이동하는
+
+  if (!moved) return {
+    result: count,
+    current: location
+  };
+  maps[moved[0]][moved[1]] = 0; // 지나온 길 0으로 변경
+
+  var _ = 재귀(maps, moved, count + 1),
+      result = _.result,
+      current = _.current;
+
+  return {
+    result: result,
+    current: current
+  };
+}
+
+function solution2(maps) {
+  var maxX = maps[0].length - 1;
+  var maxY = maps.length - 1;
+  var location = [0, 0];
+  var count = 1; // 최단거리가 아님.
+
+  var _2 = 재귀(maps, location, count),
+      result = _2.result,
+      current = _2.current;
+
+  if (current[0] === maxY && current[1] === maxX) {
+    return result;
+  }
+
+  return -1;
+}
+
+function solution(maps) {
+  var yLength = maps.length;
+  var xLength = maps[0].length;
+  var goalY = yLength - 1;
+  var goalX = xLength - 1; // y, x, steps
+
+  var queue = [];
+  queue.push([0, 0, 1]);
+
+  while (queue.length) {
+    console.log(queue);
+
+    var _queue$shift = queue.shift(),
+        _queue$shift2 = _slicedToArray(_queue$shift, 3),
+        y = _queue$shift2[0],
+        x = _queue$shift2[1],
+        steps = _queue$shift2[2];
+
+    if (x < 0 || x >= xLength) continue;
+    if (y < 0 || y >= yLength) continue;
+    if (maps[y][x] === 0) continue;
+
+    if (y === goalY && x === goalX) {
+      return steps;
+    }
+
+    maps[y][x] = 0;
+    queue.push([y + 1, x, steps + 1]);
+    queue.push([y - 1, x, steps + 1]);
+    queue.push([y, x + 1, steps + 1]);
+    queue.push([y, x - 1, steps + 1]);
+  }
+
+  return -1;
 }
 
 solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]]);
 solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 0], [0, 0, 0, 0, 1]]);
+solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 0]]);
--- DFS/게임 맵 최단거리.js
+++ DFS/게임 맵 최단거리.js
@@ -1,36 +1,92 @@
-function solution(maps) {
-  const yMax = maps.length - 1;
-  const xMax = maps[0].length - 1;
-  const directions = [
-    [0, 1],
-    [1, 0],
-    [0, -1],
-    [-1, 0],
-  ];
-  let result = -1;
-  function dfs(x, y, count) {
-    //마지막 도달하면 count 리턴
-    if (x >= xMax && y >= yMax) {
-      result = count + 1;
-      return;
+function 이동(maps, location, maxX, maxY) {
+  const direction = {
+    LEFT: [0, -1], //y,x
+    RIGHT: [0, 1],
+    DOWN: [1, 0],
+    UP: [-1, 0],
+  };
+  for (let move in direction) {
+    const 방향 = direction[move];
+    const 현재y = location[0];
+    const 현재x = location[1];
+    const 이동후 = [현재y + 방향[0], 현재x + 방향[1]];
+    if (
+      이동후[0] < 0 ||
+      이동후[1] < 0 ||
+      이동후[0] > maxY ||
+      이동후[1] > maxX
+    ) {
+      // 음수인지 아닌지 확인.
+      continue;
     }
-    //0일 경우 멈춤
-    if (!maps[y][x]) return;
+    //두가지 경우의 수가 존재하는 경우 먼저 반복되는 left right 순으로 리턴한다.
 
-    //방문 했던 곳은 발자취를 제거
-    maps[y][x] = 0;
-    for (const [dx, dy] of directions) {
-      // 모든 방향 탐색
-      const newX = x + dx;
-      const newY = y + dy;
-      if (newX >= 0 && newY >= 0 && maps[newY][newX] === 1) {
-        return dfs(newX, newY, count + 1);
-      }
+    if (maps[이동후[0]][이동후[1]] === 1) {
+      // console.log(maps[이동후[0]][이동후[1]], move);
+
+      return [이동후[0], 이동후[1]];
+    } else {
+      continue;
     }
   }
-  dfs(0, 0, 0);
-  console.log(result);
-  return result;
+}
+
+function 재귀(maps, location, count) {
+  const maxX = maps[0].length - 1;
+  const maxY = maps.length - 1;
+
+  const moved = 이동(maps, location, maxX, maxY); // 이동하는
+  if (!moved) return { result: count, current: location };
+  maps[moved[0]][moved[1]] = 0; // 지나온 길 0으로 변경
+
+  const { result, current } = 재귀(maps, moved, count + 1);
+  return { result, current: current };
+}
+
+function solution2(maps) {
+  const maxX = maps[0].length - 1;
+  const maxY = maps.length - 1;
+
+  const location = [0, 0];
+  const count = 1; // 최단거리가 아님.
+  const { result, current } = 재귀(maps, location, count);
+  if (current[0] === maxY && current[1] === maxX) {
+    return result;
+  }
+  return -1;
+}
+
+function solution(maps) {
+  const yLength = maps.length;
+  const xLength = maps[0].length;
+  const goalY = yLength - 1;
+  const goalX = xLength - 1;
+
+  // y, x, steps
+  const queue = [];
+  queue.push([0, 0, 1]);
+
+  while (queue.length) {
+    console.log(queue);
+
+    const [y, x, steps] = queue.shift();
+
+    if (x < 0 || x >= xLength) continue;
+    if (y < 0 || y >= yLength) continue;
+    if (maps[y][x] === 0) continue;
+
+    if (y === goalY && x === goalX) {
+      return steps;
+    }
+
+    maps[y][x] = 0;
+    queue.push([y + 1, x, steps + 1]);
+    queue.push([y - 1, x, steps + 1]);
+    queue.push([y, x + 1, steps + 1]);
+    queue.push([y, x - 1, steps + 1]);
+  }
+
+  return -1;
 }
 
 solution([
@@ -46,4 +102,11 @@ solution([
   [1, 0, 1, 1, 1],
   [1, 1, 1, 0, 0],
   [0, 0, 0, 0, 1],
+]);
+solution([
+  [1, 0, 1, 1, 1],
+  [1, 0, 1, 0, 1],
+  [1, 0, 1, 1, 1],
+  [1, 1, 1, 0, 1],
+  [0, 0, 0, 0, 0],
 ]);
